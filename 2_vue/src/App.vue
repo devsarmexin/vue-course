bfe4a708ccc05ac58ab2f273608545b1e3badf97
murmexin <murmexin@gmail.com>
2022-03-07 22:38:44 +0300

    начало работы, добавление App.vue

---
+++ 2_vue/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <button v-on:click="addLike">Like</button>
+        <button v-on:click="addDisLike">Dislike</button>
+    </div>
+    <div>
+        <div>Количество лайков <strong>{{ likes }}</strong></div>
+        <div>Количество дизлайков <strong>{{ dislikes }}</strong></div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 1;
+        },
+        addDisLike() {
+            this.dislikes += 1;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
