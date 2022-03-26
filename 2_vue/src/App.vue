cb8da016a620c4db2aab245ffd86af84f75f6954
murmexin <murmexin@gmail.com>
2022-03-26 18:42:26 +0300

    1.58.30 время

--- 2_vue/src/App.vue
+++ 2_vue/src/App.vue
@@ -28,19 +28,20 @@
                 v-if="!isPostsLoading"
         />
         <div v-else>Идёт загрузка...</div>
-        <div class="page__wrapper">
-            <div
-                    v-for="pageNumber in totalPage"
-                    :key="pageNumber"
-                    class="page"
-                    :class="{
-                        'current-page' : page === pageNumber
-                    }"
-                    @click="changePage(pageNumber)"
-            >
-                {{pageNumber}}
-            </div>
-        </div>
+        <div ref="observer" class="observer"></div>
+<!--        <div class="page__wrapper">-->
+<!--            <div-->
+<!--                    v-for="pageNumber in totalPage"-->
+<!--                    :key="pageNumber"-->
+<!--                    class="page"-->
+<!--                    :class="{-->
+<!--                        'current-page' : page === pageNumber-->
+<!--                    }"-->
+<!--                    @click="changePage(pageNumber)"-->
+<!--            >-->
+<!--                {{pageNumber}}-->
+<!--            </div>-->
+<!--        </div>-->
     </div>
 
 </template>
@@ -89,9 +90,9 @@
             showDialog() {
                 this.dialogVisible = true;
             },
-            changePage(pageNumber) {
-                this.page = pageNumber
-            },
+            // changePage(pageNumber) {
+            //     this.page = pageNumber
+            // },
             async fetchPosts() {
                 try {
                     this.isPostsLoading = true;
@@ -109,10 +110,39 @@
                 } finally {
                     this.isPostsLoading = false;
                 }
+            },
+            async loadMorePosts() {
+                try {
+                    this.page +=1;
+                    const response = await axios.get('https://jsonplaceholder.typicode.com/posts',
+                        {
+                            params: {
+                                _page: this.page,
+                                _limit: this._limit
+                            }
+                        });
+                    this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
+                    this.posts = [...this.posts, ...response.data];
+                } catch (e) {
+                    alert('ОШИБКА')
+                }
             }
         },
         mounted() {
             this.fetchPosts();
+            console.log(this.$refs.observer);
+            const options = {
+                rootMargin: '0px',
+                threshold: 1.0
+            }
+            const callback = (entries, observer) => {
+                if (entries[0].isIntersecting && this.page < this.totalPage) {
+                    this.loadMorePosts();
+                }
+                /* Content excerpted, show below */
+            };
+            const observer = new IntersectionObserver(callback, options);
+            observer.observe(this.$refs.observer);
         },
         computed: {
             sortedPost() {
@@ -123,9 +153,9 @@
             }
         },
         watch: {
-            page() {
-                this.fetchPosts()
-            }
+            // page() {
+            //     this.fetchPosts()
+            // }
         }
     }
 </script>
@@ -159,5 +189,9 @@
 
     .current-page {
         border: 2px solid teal;
+    }
+    .observer {
+        height: 30px;
+        background: green;
     }
 </style>
